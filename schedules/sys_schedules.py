bb1da8865ff038f4b2d4931d0791dea02e660dfa
Tian Yi <fengzhibushang@163.com>
2018-04-25 22:56:51 +0800

    add get_usage task

--- schedules/sys_schedules.py
+++ schedules/sys_schedules.py
@@ -7,7 +7,7 @@
  @Description: 
 """
 
-
 sys_schedules = [
-    ["cron", "post", "http://www.kisu.top/api/jx3/info", "get_jx3_info", "get_jx3_info", {"hour": 3}, 'sys', {}]
+    ["cron", "post", "http://www.kisu.top/api/jx3/info", "get_jx3_info", "get_jx3_info", {"hour": 3}, 'sys', {}],
+    ["cron", "post", "http://monitor.kisu.top/api/all_monitor", "get_usage", "get_usage", {"minute": '*/1'}, 'sys', {}]
 ]
